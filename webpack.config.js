de618540af56586fd6afc08e2f28249e843d1f8c
dkowalczyk <d.kowalczyk@dituel.pl>
2023-11-29 15:04:43 +0100

    header styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,74 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
+module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
+
+  return {
+    mode: isProd ? "production" : "development",
+    context: path.resolve(__dirname, "src"),
+    entry: {
+      app: "./js/index.js",
+      style: "./css/index.scss",
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: isProd ? "[name].[contenthash].js" : "[name].js",
+      publicPath: "/",
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: [{ loader: "html-loader", options: { minimize: true } }],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
           },
         },
-      },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/,
+          type: "asset/resource",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        inject: true,
+      }),
+      new MiniCssExtractPlugin({
+        filename: isProd ? "[name].[contenthash].css" : "[name].css",
+      }),
+      new CleanWebpackPlugin(),
     ],
-  },
+    devServer: {
+      headers: {
+        "Cache-Control": "no-store",
+      },
+      static: {
+        directory: path.join(__dirname, "src"),
+      },
+      compress: true,
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+    devtool: isProd ? false : "source-map",
+  };
 };
