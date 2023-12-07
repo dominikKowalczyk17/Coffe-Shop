e796fabfc949bdecfb65bf86fe0623bae1d7f31d
dkowalczyk <d.kowalczyk@dituel.pl>
2023-12-07 16:32:31 +0100

    main page

---
+++ React/coffeeshop/src/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomeSection = () => {
+  return (
+    <section className="home" id="home">
+      <div className="home__content">
+        <h3>fresh morning coffee</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <a href="" className="home__btn">
+          get yours now
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default HomeSection;
---
+++ React/coffeeshop/src/Menu.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import img1 from "./public/menu-img1.png";
+import img2 from "./public/menu-img2.png";
+import img3 from "./public/menu-img3.png";
+import img4 from "./public/menu-img4.png";
+import img5 from "./public/menu-img5.png";
+import img6 from "./public/menu-img6.png";
+
+const Menu = () => {
+  return (
+    <section className="menu" id="menu">
+      <h1 className="menu__heading">
+        our <span>menu</span>
+      </h1>
+      <div className="menu__box-container">
+        <div className="menu__box">
+          <img src={img1} alt="" className="menu__img" />
+          <h3>tasty and healthy</h3>
+          <div className="menu__price">
+            $15.99 <span>20.99</span>
+          </div>
+          <a href="#" className="menu__btn">
+            add to cart
+          </a>
+        </div>
+        <div className="menu__box">
+          <img src={img2} alt="" className="menu__img" />
+          <h3>tasty and healthy</h3>
+          <div className="menu__price">
+            $15.99 <span>20.99</span>
+          </div>
+          <a href="#" className="menu__btn">
+            add to cart
+          </a>
+        </div>
+        <div className="menu__box">
+          <img src={img3} alt="" className="menu__img" />
+          <h3>tasty and healthy</h3>
+          <div className="menu__price">
+            $15.99 <span>20.99</span>
+          </div>
+          <a href="#" className="menu__btn">
+            add to cart
+          </a>
+        </div>
+        <div className="menu__box">
+          <img src={img4} alt="" className="menu__img" />
+          <h3>tasty and healthy</h3>
+          <div className="menu__price">
+            $15.99 <span>20.99</span>
+          </div>
+          <a href="#" className="menu__btn">
+            add to cart
+          </a>
+        </div>
+        <div className="menu__box">
+          <img src={img5} alt="" className="menu__img" />
+          <h3>tasty and healthy</h3>
+          <div className="menu__price">
+            $15.99 <span>20.99</span>
+          </div>
+          <a href="#" className="menu__btn">
+            add to cart
+          </a>
+        </div>
+        <div className="menu__box">
+          <img src={img6} alt="" className="menu__img" />
+          <h3>tasty and healthy</h3>
+          <div className="menu__price">
+            $15.99 <span>20.99</span>
+          </div>
+          <a href="#" className="menu__btn">
+            add to cart
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Menu;
