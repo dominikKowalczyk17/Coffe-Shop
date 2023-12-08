a7705722e190cd998ef59c6f1ef1d43dce229596
dkowalczyk <d.kowalczyk@dituel.pl>
2023-12-08 11:20:55 +0100

    fix file structure

--- React/coffeeshop/src/App.jsx
+++ React/coffeeshop/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "./Header";
-import Home from "./Home";
-import About from "./About";
-import Menu from "./Menu";
-import Products from "./Products";
-import Review from "./Review";
-import Contact from "./Contact";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Menu from "./components/Menu";
+import Products from "./components/Products";
+import Review from "./components/Review";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import "./css/App.scss";
 
 const App = () => {
